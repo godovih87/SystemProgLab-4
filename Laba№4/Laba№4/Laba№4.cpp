4f7183957e2c3d333770009f4e104e7d5f390a5c
godovih87 <157564259+godovih87@users.noreply.github.com>
2024-11-22 23:11:20 +0500

    Add files via upload

---
+++ Laba№4/Laba№4/Laba№4.cpp
@@ -0,0 +1,40 @@
+﻿//#include <iostream>
+//#include  <math.h>
+//
+//#define T 10
+//#define Z (3 * T)
+//#define Y (cos(T + Z))
+//#define X (4 * log10(pow(Y, 2)) - (Z / (double)T))
+//
+//int main()
+//{
+//	double x = X;
+//	std::cout << "x = " << x << std::endl;
+//	printf("x = %.4f\n", x);
+//}
+
+//#include <iostream>
+//#include <math.h>
+//
+//#define X 0.27
+//#define Y 4.38
+//#define Z ((X + Y) / sqrt(X))
+//#define NUMERATOR (4 * pow(Y, 2) * exp(3 * X))
+//#define DENOMINATOR (8 * pow(Z, 3) + log(fabs(X)))
+//#define B (NUMERATOR / DENOMINATOR)
+//
+//int main() {
+//    printf("b = %.4f\n", B);
+//}
+
+//#include <iostream>
+//#include <math.h>
+//
+//int main() {
+//    double alpha = 1.0;
+//    double pi = 3.141592653589793;
+//
+//    double y = (1.0 / 4) - (1.0 / 4) * sin((5.0 / 2) * pi - 8 * alpha);
+//
+//    printf("Значение y = %.4f\n", y);
+//}
